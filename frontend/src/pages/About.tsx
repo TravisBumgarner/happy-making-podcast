84ba49577a8f6d811e4bd8fac2465c08c2d872f1
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-09-30 21:31:11 -0600

    [SSP-871] Cover Art  (#2)

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -4,7 +4,7 @@ import PageWrapper from '../sharedComponents/PageWrapper'
 
 const Contact = () => {
   return (
-    <PageWrapper width="small" minHeight staticContent>
+    <PageWrapper width="medium" minHeight staticContent>
       <Typography variant="h2">About</Typography>
       <Typography variant="body1">Coming soon.</Typography>
     </PageWrapper>
